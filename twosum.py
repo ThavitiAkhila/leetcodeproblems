bc3e2f8945c1a032ae5866833cf1da99c3df1fd5
Akhila <thavitiakhila0420@gmail.com>
2026-05-05 10:17:12 +0530

    Leetcode problem number 1

---
+++ twosum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        val_indx={}
+        for i,num in enumerate(nums):
+            if target-num in val_indx:
+                return[i,val_indx[target-num]]
+            val_indx[num]=i
